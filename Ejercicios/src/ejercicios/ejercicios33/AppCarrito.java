419711eaffd69bb00fa20ee1b5743cfb733ea148
aoumouss <abderrazak.oumouss@fpceu.es>
2026-02-09 15:17:17 +0100

    EjerciciosCasa

---
+++ Ejercicios/src/ejercicios/ejercicios33/AppCarrito.java
@@ -0,0 +1,41 @@
+package ejercicios.ejercicios33;
+
+public class AppCarrito {
+
+	public static void main(String[] args) {
+
+		
+		Cliente cliente = new Cliente("12345678X","Blas de los Montes");
+		Carrito carro = new Carrito(cliente);
+		System.out.println(carro);
+		
+		Ropa ropa = new Ropa();
+		ropa.setPrecio(20);
+		ropa.setDescripcion("Poncho");
+		ropa.setTalla("XL");
+		ropa.setColor("Azul");
+		
+		carro.addArticulo(ropa);
+		carro.addArticulo(ropa);
+		System.out.println(carro);
+		
+		
+		Libro libro = new Libro();
+		libro.setPrecio(20);
+		libro.setDescripcion("Así habló Zaratustra");
+		libro.setAutor("Nietzsche");
+		
+		carro.addArticulo(libro);
+		
+		System.out.println(carro);
+		
+		carro.borrarArticulo(1);
+		System.out.println(carro);
+
+		
+		
+		
+		
+	}
+
+}
---
+++ Ejercicios/src/ejercicios/ejercicios33/ListaArticulos.java
@@ -0,0 +1,45 @@
+package ejercicios.ejercicios33;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaArticulos {
+	
+	
+
+
+	public ListaArticulos() {
+		lista=new ArrayList<>();
+	}
+	ArrayList<Articulo> lista=new ArrayList<>();
+	
+
+	public void addArticulo(Articulo articulo) {
+		lista.add(articulo);
+	}
+	
+	
+	public Integer getCantidad() {
+		return lista.size();
+	}
+	
+	public Integer getTotal() {
+		Integer total=0;
+		for (int i = 0; i < lista.size(); i++) {
+			total+=lista.get(i).getPrecio();
+		
+		}
+		return total;
+		
+	}
+	
+	public void borrarArticulo(Integer posicion) {
+		lista.remove(posicion);
+	}
+	
+	public void vaciarCesta() {
+		lista.clear();
+	}
+	
+	
+}
